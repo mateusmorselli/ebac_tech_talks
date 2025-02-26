6349a56c7f7f3b18fdb58d3168e3f6ccf7926a30
Mateus Morselli <matmorselli@gmail.com>
2025-02-26 00:22:58 -0300

    contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('ola mundo')
+AOS.init();
+
+const dataDoEvento = new Date("Aug 23, 2025 12:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / (1000*60*60*24));
+    const horasAteEvento = Math.floor((distanciaAteEvento % (1000*60*60*24))/(1000*60*60));
+    const minutosAteEvento = Math.floor((distanciaAteEvento % (1000*60*60))/(1000*60));
+    const segundosAteEvento = Math.floor((distanciaAteEvento % (1000*60))/1000);
+    console.log(diasAteEvento, horasAteEvento, minutosAteEvento, segundosAteEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d, ${horasAteEvento}h, ${minutosAteEvento}m e ${segundosAteEvento}s`;
+
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `Evento expirado!`
+    }
+}, 1000);
